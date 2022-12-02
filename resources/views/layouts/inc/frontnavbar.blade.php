f955ad0903b1931c028c628f7aa94e45609e716e
Kazi Md. Al-Wakil <kazimdalwakil@gmail.com>
2022-12-02 21:06:46 +0600

    Proceed to checkou and User Order view

--- resources/views/layouts/inc/frontnavbar.blade.php
+++ resources/views/layouts/inc/frontnavbar.blade.php
@@ -42,9 +42,6 @@
                         <a class="dropdown-item" href="{{ url('cart') }}">
                             My Cart
                         </a>
-                        <a class="dropdown-item" href="{{ url('/dashboard') }}">
-                            Dashboard
-                        </a>
                         <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                             {{ __('Logout') }}
