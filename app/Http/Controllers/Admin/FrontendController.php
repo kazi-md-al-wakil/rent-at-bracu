bfaa8a436b8f9b52bb727798ad4f90110d5f284c
Kazi Md. Al-Wakil <kazimdalwakil@gmail.com>
2022-12-04 20:57:08 +0600

    added useers in admind dashboard

--- app/Http/Controllers/Admin/FrontendController.php
+++ app/Http/Controllers/Admin/FrontendController.php
@@ -4,11 +4,17 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+
+use function Ramsey\Uuid\v1;
 
 class FrontendController extends Controller
 {
     public function index()
     {
-        return view('admin.index');
+        $users=User::all();
+        $orders=Order::all();
+        return view('admin.index',compact('users, orders'));
     }
 }
+
